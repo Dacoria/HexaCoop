64d746d2d224c552e680ba93ccb6e43a7bbbedb0
Dac <Dacoria@gmail.com>
2022-08-19 00:00:45 +0200

    .

---
+++ HexaCoop/Assets/Imported/_Unused/ImportedImagesAbilities/UIRevealTileMapScript.cs
@@ -0,0 +1,37 @@
+﻿using Chibi.Free;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRevealTileMapScript : HexaEventCallback
+{
+    [ComponentInject] private Button button;
+
+    private new IEnumerator Start()
+    {
+        base.Start();
+        yield return new WaitForSeconds(0.3f); 
+
+        if (Settings.ShowOnlyPlainHexesBeforeGameStart)
+        {
+            MonoHelper.instance.SetHexLayoutBeforeGame(originalLayout: false); 
+        }
+        else 
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    protected override void OnNewRoundStarted(List<PlayerScript> allPlayers, PlayerScript player)
+    {
+        Destroy(gameObject);
+    }
+
+    public void OnButtonClick()
+    {
+        button.interactable = false;
+        MonoHelper.instance.SetHexLayoutBeforeGame(true);
+        Destroy(gameObject);
+    }    
+}
